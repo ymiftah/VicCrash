539734b9d6cf17994bea5fd44e37ff14b0243939
youssefmiftah <miftahyo@outlook.fr>
2022-02-16 20:23:34 +0100

    update

--- utils.py
+++ utils.py
@@ -17,39 +17,27 @@ df = df[['ACCIDENT_NO', 'Accident Type Desc', 'Day Week Description',
        'DCA Description', 'Light Condition Desc', 'Road Geometry Desc', 'SEVERITY',
        'SPEED_ZONE', 'ACCIDENTYEAR', 'ACCIDENTHOUR']]
 
-foo = pd.read_csv(files.open('NODE.csv')).drop_duplicates(subset=['ACCIDENT_NO'])
-df = df.merge(foo[['ACCIDENT_NO', 'LGA_NAME', 'Lat', 'Long']],
+foo = pd.read_csv(files.open('NODE.csv'), usecols = ['ACCIDENT_NO', 'LGA_NAME', 'Lat', 'Long']).drop_duplicates(subset=['ACCIDENT_NO'])
+df = df.merge(foo,
     left_on='ACCIDENT_NO',right_on='ACCIDENT_NO', how='inner')
 
-foo = pd.read_csv(files.open('ROAD_SURFACE_COND.csv')).drop_duplicates(subset=['ACCIDENT_NO'])
-df = df.merge(foo[['ACCIDENT_NO', 'SURFACE_COND', 'Surface Cond Desc']], left_on='ACCIDENT_NO',right_on='ACCIDENT_NO', how='left')
+foo = pd.read_csv(files.open('ROAD_SURFACE_COND.csv'), usecols=['ACCIDENT_NO', 'SURFACE_COND', 'Surface Cond Desc']).drop_duplicates(subset=['ACCIDENT_NO'])
+df = df.merge(foo, left_on='ACCIDENT_NO',right_on='ACCIDENT_NO', how='left')
 
-foo = pd.read_csv(files.open('ACCIDENT_LOCATION.csv'))
-df = df.merge(foo[['ACCIDENT_NO', 'ROAD_NAME', 'ROAD_TYPE', 'ROAD_NAME_INT', 'ROAD_TYPE_INT']], left_on='ACCIDENT_NO',right_on='ACCIDENT_NO', how='left')
+foo = pd.read_csv(files.open('ACCIDENT_LOCATION.csv'), usecols=['ACCIDENT_NO', 'ROAD_NAME'])
+df = df.merge(foo, left_on='ACCIDENT_NO',right_on='ACCIDENT_NO', how='left')
 
-foo = pd.read_csv(files.open('VEHICLE.csv'))
-foo = foo[['ACCIDENT_NO', 'VEHICLE_YEAR_MANUF']]
-foo.drop(foo[foo['VEHICLE_YEAR_MANUF'] < 1900].index, inplace=True)
+foo = pd.read_csv(files.open('VEHICLE.csv'), usecols=['ACCIDENT_NO', 'VEHICLE_YEAR_MANUF'])
+foo = foo[foo['VEHICLE_YEAR_MANUF'] < 1900]
 foo = foo.groupby('ACCIDENT_NO').min().reset_index()
 df = df.merge(foo[['ACCIDENT_NO', 'VEHICLE_YEAR_MANUF']], left_on='ACCIDENT_NO',right_on='ACCIDENT_NO', how='left')
 
-foo = pd.read_csv(files.open('SUBDCA.csv'))
-foo = foo[['ACCIDENT_NO', 'Sub Dca Code Desc']]
+foo = pd.read_csv(files.open('SUBDCA.csv'), usecols=['ACCIDENT_NO', 'Sub Dca Code Desc'])
 foo = foo.groupby('ACCIDENT_NO').agg(','.join).reset_index()
 df = df.merge(foo[['ACCIDENT_NO', 'Sub Dca Code Desc']], left_on='ACCIDENT_NO',right_on='ACCIDENT_NO', how='left')
 
 del(foo)
 
-select = ['ACCIDENT_NO', 'ACCIDENTDATE', 'ACCIDENTTIME',
-       'Accident Type Desc', 'Day Week Description',
-       'DCA Description',  'Light Condition Desc',
-       'NO_OF_VEHICLES', 'NO_PERSONS', 'NO_PERSONS_INJ_2',
-       'NO_PERSONS_INJ_3', 'NO_PERSONS_KILLED', 'NO_PERSONS_NOT_INJ',
-       'POLICE_ATTEND', 'Road Geometry Desc', 'SEVERITY',
-       'SPEED_ZONE', 'ACCIDENTYEAR', 'ACCIDENTHOUR', 'LGA_NAME', 'Lat', 'Long',
-       'Surface Cond Desc', 'ROAD_NAME', 'ROAD_TYPE',
-       'ROAD_NAME_INT', 'ROAD_TYPE_INT']
-df = df[select]
 df['crashes'] = 1
 df = df[df.SPEED_ZONE < 200]
 
